42839e938d2dd116676fd196b3470859b788ddb2
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-22 18:59:39 +0200

    added game.js
    in game.js added array,  pair of colors

---
+++ Memory/game.js
@@ -0,0 +1,1 @@
+const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
