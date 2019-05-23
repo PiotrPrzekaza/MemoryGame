ac8b5193812177307be90f2ffce5edabc6fab9fc
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-23 14:27:51 +0200

    set timeout function, hide all card colors and initial addeventlisinet function

--- Memory/game.js
+++ Memory/game.js
@@ -3,12 +3,23 @@ const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "br
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
-const init = function () {
-    cards.forEach(function (card) {
+
+const clickCard = () => {
+    console.log("klik");
+}
+const init = () => {
+    cards.forEach(card => {
         const position = Math.floor(Math.random() * cardsColor.length);
         card.classList.add(cardsColor[position]);
         cardsColor.splice(position, 1);
     })
+
+    setTimeout(function () {
+        cards.forEach(function (card) {
+            card.classList.add("hidden")
+            card.addEventListener("click", clickCard)
+        })
+    }, 1000)
 }
 
 init();
