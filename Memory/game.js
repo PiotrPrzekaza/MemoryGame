a3f28cef67653ad2ec07ecaa694d17849e927983
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-23 13:54:31 +0200

    Swap all divs from NodeList to Array

--- Memory/game.js
+++ Memory/game.js
@@ -1,1 +1,4 @@
 const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
+
+let cards = document.querySelectorAll("div");
+cards = [...cards];
