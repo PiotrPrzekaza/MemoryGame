8d86a183786a6a4063d9e64095205c28d67a3eed
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-23 15:48:53 +0200

    comparison two cards and adding appropriate css class to hide or off

--- Memory/game.js
+++ Memory/game.js
@@ -7,7 +7,7 @@ const timeStart = new Date().getTime();
 
 
 let activeCard = "";
-const activPair = [];
+const activePair = [];
 
 const gamePairs = cards.length / 2
 let gameResult = 0;
@@ -20,7 +20,26 @@ const clickCard = function () {
         activePair[0] = activeCard;
         return;
     }
+    else {
+        cards.forEach(card => card.removeEventListener("click", clickCard))
+        activePair[1] = activeCard;
+
+        setTimeout(function () {
+            if (activePair[0].className === activePair[1].className) {
+                activePair.forEach(card => {
+                    card.classList.add('off');
+                })
+            }
+            else {
+                activePair.forEach(card => {
+                    card.classList.add('hidden')
+                })
+            }
+        }, 1000)
+
+    };
 }
+
 const init = () => {
     cards.forEach(card => {
         const position = Math.floor(Math.random() * cardsColor.length);
